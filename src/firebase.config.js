bf1e80bb55c0a4af1912e415c62c7c59a959e7ec
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-28 18:50:32 +0100

    User able to sign up and it will create a document on firebase with its credentials on "users" collection

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore/lite";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
