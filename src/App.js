ffd9fed211a798132b2b655098a66b4fd67954bc
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-28 17:35:53 +0100

    Pages created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
     <div>
-      <h1>myapp</h1>
+      <h1>Cork House Marketplace</h1>
     </div>
   );
 }
