eac3b5ecfb2d04b9130d8e992d5ba37506ef06c3
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-29 20:48:46 +0100

    create a react hook called useAuthStatus that will check if we are logged in, if so, we can go to our profile, and if we log out it wont crash the profile section because when we click the profile button it will redirect the user to sign-in route to sign-in or sign-up

---
+++ src/hooks/useAuthStatus.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export const useAuthStatus = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setCheckingStatus] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setCheckingStatus(false);
+    });
+  });
+
+  return { loggedIn, checkingStatus };
+};
+//  https://stackoverflow.com/questions/65505665/protected-route-with-firebase
+
+// Fix memory leak warning
+// https://stackoverflow.com/questions/59780268/cleanup-memory-leaks-on-an-unmounted-component-in-react-hooks
