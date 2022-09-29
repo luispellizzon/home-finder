587f345888455d02be06fd4c7eb62cc701ae8d76
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-29 20:52:58 +0100

    Memory leak happening after mounting the profile. Sorted using hook useRef

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,22 +1,31 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [checkingStatus, setCheckingStatus] = useState(true);
+  const _isMounted = useRef(true);
 
   useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true);
-      }
-      setCheckingStatus(false);
-    });
-  });
+    if (_isMounted) {
+      const auth = getAuth();
+
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true);
+        }
+        setCheckingStatus(false);
+      });
+    }
+    return () => {
+      _isMounted.current = false;
+    };
+  }, [_isMounted]);
 
   return { loggedIn, checkingStatus };
 };
+
+// Protected Routes v6
 //  https://stackoverflow.com/questions/65505665/protected-route-with-firebase
 
 // Fix memory leak warning
