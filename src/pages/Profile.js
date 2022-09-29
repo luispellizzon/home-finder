d3917157e50566c9dda4b124e33ff3b11cc13d80
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-29 19:41:00 +0100

    When Sign In, redirects to Profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { getAuth } from "firebase/auth";
 
 function Profile() {
-  return <div>Profile</div>;
+  const [user, setUser] = useState(null);
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    console.log(auth.currentUser);
+    setUser(auth.currentUser);
+  }, []);
+
+  return user ? <h1>{user.displayName}</h1> : "not logged in";
 }
 
 export default Profile;
