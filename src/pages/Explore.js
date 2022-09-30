754a3c011a37a2776830e1d35570fa87bdd43e58
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-30 21:40:57 +0100

    Explore page being built with images and creating page for each category: sale or rent

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,7 +1,38 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
+import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
 
 function Explore() {
-  return <div>Explore</div>;
+  return (
+    <div className="explore">
+      <header>
+        <p className="pageHeader">Explore</p>
+      </header>
+      <main>
+        {/* Slider goes here */}
+
+        <p className="exploreCategoryHeading">Categories</p>
+        <div className="exploreCategories">
+          <Link to="/category/rent">
+            <img
+              src={rentCategoryImage}
+              alt="Rent"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for rent</p>
+          </Link>
+          <Link to="/category/sell">
+            <img
+              src={sellCategoryImage}
+              alt="Sell"
+              className="exploreCategoryImg"
+            />
+            <p className="exploreCategoryName">Places for sale</p>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Explore;
