3b68df9557e6981243444bdf697d9acde2b27c7b
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-30 20:23:19 +0100

    Importing to start ForgotPassword.js page

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,4 +1,8 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { getAuth, sendPasswordResetEmail } from "firebase/auth";
+import { toast } from "react/toastify";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 
 function ForgotPassword() {
   return <div>Forgot Password</div>;
