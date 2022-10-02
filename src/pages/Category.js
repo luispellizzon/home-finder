e8331c322922bea8ed1175eb1273d951039a1504
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-02 17:49:16 +0100

    Importing stuff to start Category page

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,6 +1,26 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  limit,
+  startAfter,
+} from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from "react-toastify";
+import Spinner from "../components/Spinner";
 
 function Category() {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  useEffect(() => {}, []);
+
   return <div>Category</div>;
 }
 
