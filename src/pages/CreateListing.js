fc908379186217ff78d0da2ab44f877670e25624
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-12 18:51:35 +0100

    Submit function created and checking inputs before submiting

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -2,6 +2,7 @@ import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
+import { toast } from "react-toastify";
 
 function CreateListing() {
   const [geolocationEnable, setGeolocationEnable] = useState(true);
@@ -59,7 +60,18 @@ function CreateListing() {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
+    setLoading(true);
+
+    if (discountedPrice >= regularPrice) {
+      setLoading(false);
+      toast.error("Discounted price needs to be less than regular price");
+      return;
+    }
+    if (images.length > 6) {
+      setLoading(false);
+      toast.error("Max 6 images per listing");
+      return;
+    }
   };
   const onMutate = (e) => {
     // Check true or false
