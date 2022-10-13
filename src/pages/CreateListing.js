90a9a37460aa06693957a219361ccdb283ffc505
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-13 14:30:50 +0100

    Uploading images to firebase storage success

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -7,6 +7,7 @@ import {
   getDownloadURL,
 } from "firebase/storage";
 import { db } from "../firebase.config";
+import { v4 as uuidv4 } from "uuid";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { toast } from "react-toastify";
@@ -114,6 +115,54 @@ function CreateListing() {
       geolocation.lng = longitude;
       location = address;
     }
+
+    //Store images on firebase
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+
+        const storageRef = ref(storage, "images/" + fileName);
+        const uploadTask = uploadBytesResumable(storageRef, image);
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress =
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log("upload is" + progress + " % done");
+
+            switch (snapshot.state) {
+              case "paused":
+                console.log("upload is paused");
+                break;
+              case "running":
+                console.log("upload is running");
+                break;
+              default:
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>
+              resolve(downloadURL)
+            );
+          }
+        );
+      });
+    };
+
+    const imgUrls = await Promise.all(
+      [...images].map((image) => storeImage(image))
+    ).catch(() => {
+      setLoading(false);
+      toast.error("Images not uploaded");
+      return;
+    });
+
+    console.log(imgUrls);
 
     setLoading(false);
   };
