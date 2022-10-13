8c28a9fec9ef17cd3ef0c99c84163bcd2dcc753f
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-13 15:05:03 +0100

    Able to create Listings and store at firebase storage, and our app will fetch the listing according to its type

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -6,6 +6,7 @@ import {
   uploadBytesResumable,
   getDownloadURL,
 } from "firebase/storage";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { v4 as uuidv4 } from "uuid";
 import { useNavigate } from "react-router-dom";
@@ -99,12 +100,12 @@ function CreateListing() {
           ? undefined
           : data.results[0]?.formatted_address;
 
-      setFormData((prevState) => ({
-        ...prevState,
-        address: location,
-        latitude: geolocation.lat,
-        longitude: geolocation.lng,
-      }));
+      //   setFormData((prevState) => ({
+      //     ...prevState,
+      //     address: location,
+      //     latitude: geolocation.lat,
+      //     longitude: geolocation.lng,
+      //   }));
 
       if (location === undefined || location.includes("undefined")) {
         setLoading(false);
@@ -162,9 +163,22 @@ function CreateListing() {
       return;
     });
 
-    console.log(imgUrls);
+    const formDataCopy = {
+      ...formData,
+      imgUrls,
+      geolocation,
+      timestamp: serverTimestamp(),
+    };
 
+    delete formDataCopy.images;
+    delete formDataCopy.address;
+    location && (formDataCopy.location = location);
+    !formDataCopy.offer && delete formDataCopy.discountedPrice;
+
+    const docRef = await addDoc(collection(db, "listings"), formDataCopy);
     setLoading(false);
+    toast.success("Listing saved");
+    navigate(`/category/${formDataCopy.type}/${docRef.id}`);
   };
 
   const onMutate = (e) => {
