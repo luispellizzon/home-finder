d4bcb54de2007ebdf390d5833f92abdd5a076712
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-13 14:04:30 +0100

    Importing storage stuff from firebase to upload images, and installing a package called uuid to give each image a unique id

--- src/pages/CreateListing.js
+++ src/pages/CreateListing.js
@@ -1,5 +1,12 @@
 import { useState, useEffect, useRef } from "react";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
+import { db } from "../firebase.config";
 import { useNavigate } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { toast } from "react-toastify";
