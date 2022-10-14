ac50d002c125d6c48562ee601f061519c8b14d5a
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-14 17:33:58 +0100

    deleting location on url params query

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -86,7 +86,7 @@ function Listing() {
         {/* Map goes here */}
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
-            to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`}
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
             className="primaryButton"
           >
             Contact Landlord
