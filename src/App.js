69fa707387ef249cadb2ae32be2b67a93f78ebdf
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-14 17:35:46 +0100

    Contact Landlord Page, and added route to it on app.js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import Category from "./pages/Category";
 import CreateListing from "./pages/CreateListing";
 import Listing from "./pages/Listing";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
             path="category/:categoryName/:listingId"
             element={<Listing />}
           />
+          <Route path="/contact/:landlordId" element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
