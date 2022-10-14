17cb0ddbc5df8f7bf5a6b3f19e50046a656942d3
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-14 16:23:09 +0100

    Initializing functions for params, auth and navigate'

--- src/pages/Listing.js
+++ src/pages/Listing.js
@@ -4,8 +4,17 @@ import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
 import Spinner from "../components/Spinner";
+import sharedIcon from "../assets/svg/shareIcon.svg";
 
 function Listing() {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [sharedLinkCopied, setSharedLinkCopied] = useState(false);
+
+  const navigate = useNavigate();
+  const params = useParams();
+  const auth = getAuth();
+
   return <div>Listing</div>;
 }
 
