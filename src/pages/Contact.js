66b12e2efd8307adbc3f2bcf43adafd5e1fa36cd
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-10-15 21:43:24 +0100

    params never used, eslint disable next line

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,8 @@ import { toast } from "react-toastify";
 function Contact() {
   const [message, setMessage] = useState("");
   const [landlord, setLandlord] = useState(null);
-  const [searchParams] = useSearchParams();
+  //eslint-disable-next-line
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const params = useParams();
 
